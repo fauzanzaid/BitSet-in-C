a6d52741cb971867975a864908bb5649c3ca486a
fauzanzaid <fauzanzaid.dev@gmail.com>
2018-02-13 16:25:55 +0530

    Create function protypes

---
+++ include/BitSet.h
@@ -0,0 +1,86 @@
+#ifndef 9B590AB7DDEA4B46AA1D708278DE34E8
+#define 9B590AB7DDEA4B46AA1D708278DE34E8
+
+
+/////////////////////
+// Data Structures //
+/////////////////////
+
+typedef struct BitSet BitSet;
+
+
+//////////////////////////////////
+// Constructors and Destructors //
+//////////////////////////////////
+
+/**
+ * Allocates memory and returns pointer to BitSet struct
+ * @param  index_lo Lowest index
+ * @param  index_hi Highest index
+ * @return          Pointer to BitSet struct
+ */
+BitSet *BitSet_new(int index_lo, int index_hi);
+
+/**
+ * Allocates memory and returns a pointer to a clone of another BitSet struct
+ * passed as a pointer
+ * @param  set_ptr Pointer to struct which is to be copied
+ * @return         Pointer to new, copied struct
+ */
+BitSet *BitSet_clone(BitSet *set_ptr);
+
+/**
+ * Deallocates all internal memory assigned to the BitSet struct
+ * @param set_ptr Pointer to BitSet struct
+ */
+void BitSet_destroy(BitSet *set_ptr);
+
+
+///////////////////////////
+// Operate on single set //
+///////////////////////////
+
+void BitSet_set_index(BitSet *set_ptr, int index, int value);
+
+void BitSet_flip_index(BitSet *set_ptr, int index);
+
+void BitSet_set_all(BitSet *set_ptr, int value);
+
+void BitSet_not(BitSet *set_ptr);
+
+
+/////////////////////////
+// Operate on two sets //
+/////////////////////////
+
+void BitSet_and(BitSet *base_set_ptr, BitSet *other_set_ptr);
+
+void BitSet_or(BitSet *base_set_ptr, BitSet *other_set_ptr);
+
+void BitSet_xor(BitSet *base_set_ptr, BitSet *other_set_ptr);
+
+void BitSet_difference(BitSet *base_set_ptr, BitSet *other_set_ptr);
+
+
+//////////////
+// Retrieve //
+//////////////
+
+int BitSet_get_index(BitSet *set_ptr, int index);
+
+/**
+ * Check if any element is set to 1
+ * @param  set_ptr Pointer to BitSet struct
+ * @return         0 or 1
+ */
+int BitSet_get_any(BitSet *set_ptr);
+
+/**
+ * Check if all elements are set to 1
+ * @param  set_ptr Pointer to BitSet struct
+ * @return         0 or 1
+ */
+int BitSet_get_all(BitSet *set_ptr);
+
+
+#endif
